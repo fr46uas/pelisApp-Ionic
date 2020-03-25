9f361a899b022e2caad85d0a667568c353282575
FR46UAS <sergio.fraguas@gmail.com>
2020-03-25 19:40:55 +0100

    create model + service pelis

--- src/app/services/peli.service.ts
+++ src/app/services/peli.service.ts
@@ -2,11 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { iPelis } from '../model/iPelis.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
+
+  searchMovie() {
+
+  }
+
+  getDetails() {
+
+  }
 }
